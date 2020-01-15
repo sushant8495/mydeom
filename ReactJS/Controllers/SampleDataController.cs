8d1610c60a48cfba959fd7bf465bafdecccd112f
sushant8495 <igapsushant@gmail.com>
2020-01-15 23:01:46 +0530

    InitallCommit

--- ReactJS/Controllers/SampleDataController.cs
+++ ReactJS/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace ReactJS.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching"
         };
 
         [HttpGet("[action]")]
